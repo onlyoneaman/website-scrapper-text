da0cc162173fa4123194d8e0d32c362fff1da710
Aman <2000.aman.sinha@gmail.com>
2023-04-30 15:05:02 +0530

    Basic Scrapper

---
+++ helpers.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+
+
+def get_root_url(url):
+    """
+    Extracts the root URL from a given URL by removing any path segments and query parameters.
+    """
+    parsed_url = urlparse(url)
+    scheme = parsed_url.scheme if parsed_url.scheme else 'https'
+    netloc = parsed_url.netloc if parsed_url.netloc else parsed_url.path.split('/')[0]
+    root_url = f'{scheme}://{netloc}'
+    return root_url
+
+
+def get_full_path(url):
+    """
+    Takes a URL as input and returns a string with the domain and path parts joined by dashes.
+    """
+    # Remove the scheme (http:// or https://) from the URL
+    if url.startswith('http://'):
+        url = url[7:]
+    elif url.startswith('https://'):
+        url = url[8:]
+
+    # Remove any trailing slashes (/) from the URL
+    url = url.rstrip('/')
+
+    # Replace any remaining slashes (/) with dashes (-)
+    url = url.replace('/', '-')
+
+    return url
+
+
+def get_base_url(url):
+    """
+    Extracts the base URL from a given URL by removing the protocol and any path segments.
+    """
+    parsed_url = urlparse(url)
+    base_url = parsed_url.netloc
+    if base_url.startswith('www.'):
+        base_url = base_url[4:]
+    return base_url
+
+
+def get_all_sitemap_urls(base_url, sitemap_url):
+    """
+    Recursively fetches all sitemap URLs from the provided sitemap URL.
+    Returns a list of all site URLs found in the sitemaps.
+    """
+    site_urls = []
+    response = requests.get(sitemap_url)
+    soup = BeautifulSoup(response.text, 'xml')
+    sitemap_tags = soup.find_all('sitemap')
+    for sitemap_tag in sitemap_tags:
+        loc_tag = sitemap_tag.find('loc')
+        if loc_tag:
+            sitemap_url = urljoin(base_url, loc_tag.text)
+            site_urls += get_all_sitemap_urls(base_url, sitemap_url)
+    urlset_tags = soup.find_all('urlset')
+    for urlset_tag in urlset_tags:
+        url_tags = urlset_tag.find_all('url')
+        for url_tag in url_tags:
+            loc_tag = url_tag.find('loc')
+            if loc_tag:
+                site_urls.append(loc_tag.text)
+    return site_urls
+
+
+def get_all_sites(base_url):
+    """
+    Fetches all sitemap URLs from the base URL and recursively fetches all site URLs from the sitemaps.
+    Returns a list of all site URLs found.
+    """
+    site_urls = []
+    if not base_url.startswith('http') and not base_url.startswith('https'):
+        base_url = 'https://' + base_url
+    sitemap_url = urljoin(base_url, '/sitemap.xml')
+    site_urls += get_all_sitemap_urls(base_url, sitemap_url)
+    return site_urls
---
+++ web_scrapper.py
@@ -0,0 +1,99 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from helpers import get_all_sites, get_full_path, get_root_url, get_base_url
+import time
+import argparse
+import concurrent.futures
+from termcolor import colored
+import math
+
+
+def fetch_website_info(url):
+    """
+    Fetches website info and returns the BeautifulSoup object.
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+
+def save_text(soup, directory, filename):
+    """
+    Extracts the text from the website and saves it to a file in the specified directory.
+    """
+    text = soup.get_text()
+    with open(os.path.join(directory, filename), 'w', encoding='utf-8') as file:
+        file.write(text)
+
+
+def fetch_page(url, directory, current, total, debug):
+    print(f'Fetching page {current + 1} / {total}: {url}') if debug else None
+    start_time = time.time()
+    soup = fetch_website_info(url)
+    filename = get_full_path(url) + '.txt'
+    save_text(soup, directory, filename)
+    end_time = time.time()
+    print(f'Fetched and saved {url} in {"{:.2f}".format(end_time - start_time)} seconds.') if debug else None
+
+
+def fetch_pages(base_url, num_pages, max_workers=None,debug=False):
+    """
+    Fetches the specified number of pages from the site and saves them to files in a directory named after the base URL.
+    """
+    start_time = time.time()
+    directory = os.path.join('public', get_base_url(base_url))
+    os.makedirs(directory, exist_ok=True)
+    site_urls = get_all_sites(base_url)
+    total_urls = len(site_urls)
+    num_pages = num_pages if num_pages else total_urls
+    print(colored('Fetching {} pages from {}...'.format(num_pages, base_url), 'yellow'))
+
+    if max_workers is None:
+        max_workers = min(math.ceil(num_pages / 10), 10)
+        max_workers = max(max_workers, 100)
+
+    print(colored('Using {} workers.'.format(max_workers), 'yellow'))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for url_index in range(total_urls):
+            url = site_urls[url_index]
+            if url_index >= num_pages:
+                print(f'Fetched {num_pages} pages. Leaving remaining {total_urls - num_pages} pages.')
+                break
+            futures.append(executor.submit(fetch_page, url, directory, url_index, total_urls, debug))
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+    end_time = time.time()
+    total_time = "{:.2f}".format(end_time - start_time)
+    print(colored(f'{num_pages} pages fetched and saved to public/{get_base_url(base_url)} directory.', 'green'))
+    print(colored(f'Total time taken: {total_time} seconds.', 'green'))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Fetch website pages and create knowledge base')
+    parser.add_argument('url', metavar='URL', type=str, nargs='?', const=None, help='website URL')
+    parser.add_argument('--no-limit', '-nl', action='store_true', help='fetch all available pages')
+    parser.add_argument('--debug', '-d', action='store_true', help='shows all debug messages')
+    args = parser.parse_args()
+    url = args.url
+    debug = args.debug
+    num_pages = None
+    if not args.debug:
+        debug = False
+    if not args.no_limit:
+        num_pages = input('Enter number of pages (default: 3): ')
+        num_pages = int(num_pages) if num_pages else 3
+    if not url:
+        url = input('Enter website URL: ')
+    if url is None or url == '':
+        print(colored('URL cannot be empty.', 'red'))
+        return
+    base_url = get_root_url(url)
+    print("Fetching pages from '{}'...".format(base_url))
+    fetch_pages(base_url, num_pages, debug=debug)
+
+
+if __name__ == '__main__':
+    main()
